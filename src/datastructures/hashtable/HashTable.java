7525891961c9e9cc1265eb0bbed520983b8033f8
dimit <dimitriskyriakoglou@gmail.com>
2024-02-18 23:05:38 +0200

    v1

--- src/datastructures/hashtable/HashTable.java
+++ src/datastructures/hashtable/HashTable.java
@@ -1,5 +1,8 @@
 package datastructures.hashtable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HashTable {
     private int size = 7;
     private Node[] dataMap;
@@ -15,6 +18,18 @@ public class HashTable {
         Node(String key, int value){
             this.key=key;
             this.value=value;
+        }
+    }
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            if(dataMap[i] != null) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
         }
     }
     private int hash(String key)
@@ -46,12 +61,31 @@ public class HashTable {
         int index = hash(key);
         Node temp = dataMap[index];
         while(temp.next !=null){
-            if (temp.key == key ){return temp.value;}
+            if (temp.key == key )return temp.value;
             temp = temp.next;
         }
         return 0;
     }
 
+    public ArrayList keys() {
+        ArrayList<String> allKeys = new ArrayList<>();
+        for(int i=0; i<dataMap.length; i++){
+            Node temp = dataMap[i];
+            while(temp  !=null){
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+        return allKeys;
+
+    }
+
+    public static boolean itemInCommon(int[] array1, int[] array2){
+       HashMap<Integer,Boolean> myHashMap = new HashMap<>();
+       for(int i : array1){myHashMap.put(i,true);}
+       for (int j :array2){if (myHashMap.get(j)!= null)return true;}
+       return false;
+    }
 
 
 
